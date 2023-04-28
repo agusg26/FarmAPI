15daed018dfefcf8c7a84703d4c70c4dfce04de0
Agustin <agusg2600@gmail.com>
2023-04-28 20:25:29 -0300

    Main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from Routers import authUser,Product
+from fastapi.templating import Jinja2Templates
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 app.include_router(authUser.router)
 app.include_router(Product.router)
 
 @app.get("/")
-async def inicio():
-    return "Bienvenido"
+async def inicio(request: Request):
+    return templates.TemplateResponse("index.html",{"request": request})
