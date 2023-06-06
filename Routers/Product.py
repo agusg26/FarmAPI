25c5e96074d15f7220c1f847563c2d5625c401fa
Agustin <agusg2600@gmail.com>
2023-06-06 19:18:59 -0300

    Product

--- Routers/Product.py
+++ Routers/Product.py
@@ -14,7 +14,8 @@ router = APIRouter(prefix= "/product",tags=["product"])
 
 
 
-@router.post("/new")
+
+@router.post("/new",response_model=Product, status_code=status.HTTP_201_CREATED)
 async def addProduct(product: Product, user: User = Depends(current_user)):
     if user is None:
         raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED,
@@ -28,19 +29,19 @@ async def addProduct(product: Product, user: User = Depends(current_user)):
     id = client.products.insert_one(product_dict).inserted_id
     return search_product("_id",id)
 
-@router.get("/all")
+@router.get("/all", status_code=status.HTTP_200_OK)
 async def getProducts(request: Request):
     list = products_schema(client.products.find())
     return templates.TemplateResponse("products.html",{"request": request, "products": list})
 
-@router.get("/{id}")
+@router.get("/{id}",status_code=status.HTTP_302_FOUND)
 async def getProduct(id: str, user: User = Depends(current_user)):
     if user is None:
         raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED,
                             detail= "Usted no esta registrado como usuario")
     return search_product("_id", ObjectId(id))
 
-@router.post("/")
+@router.post("/",status_code=status.HTTP_302_FOUND)
 async def getProduct(request: Request):
     formdata = await request.form()
     name = formdata['name']
@@ -59,19 +60,32 @@ async def stock(name: str,cant: int ,user: User = Depends(current_user)):
     client.products.update_one({"name": name},{"$set": {"last_update": datetime.now()}})
     return search_product("name",name)
 
+@router.post("/update")
+async def update(request: Request):
+    data = await request.form()
+    product_dict = dict(data)
+    del product_dict["prod_name"]
+    try:
+        search_product("name",data["prod_name"])
+        client.products.find_one_and_replace({"name": data["prod_name"]},product_dict)
+        client.products.update_one({"name": product_dict["name"]},{"$set": {"last_update": datetime.now()}})
+    except:
+        return templates.TemplateResponse("errorupdate.html",{"request":request})
+    return RedirectResponse("/",303)
+
 @router.patch("/update_price/{name}")
-async def price(name: str,cant: int, user: User = Depends(current_user)):
+async def price(name: str,price: int,user: User = Depends(current_user)):
     if user is None:
         raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED,
                             detail= "Usted no esta registrado como usuario")
     if type(search_product("name",name)) is not Product:
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
                             detail= "Producto no encontrado")
-    client.products.update_one({"name": name},{"$inc": {"price": cant}})
+    client.products.update_one({"name": name},{"$inc": {"price": price}})
     client.products.update_one({"name": name},{"$set": {"last_update": datetime.now()}})
     return search_product("name",name)
 
-@router.delete("/{id}")
+@router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT)
 async def product_del(id: str, user: User = Depends(current_user)):
     if user is None:
         raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED,
