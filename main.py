6d6b816275bd6fd80f2207a766f2bb13998e4250
Agustin <agusg2600@gmail.com>
2023-06-06 19:32:43 -0300

    Main

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ templates = Jinja2Templates(directory="templates")
 app.include_router(authUser.router)
 app.include_router(Product.router)
 
+
 @app.get("/")
 async def inicio(request: Request):
     return templates.TemplateResponse("index.html",{"request": request})
+
+@app.get("/loginform")
+async def login(request: Request):
+    return templates.TemplateResponse("login.html",{"request": request})
+
+@app.get("/updateform")
+async def login(request: Request):
+    return templates.TemplateResponse("update.html",{"request": request})
